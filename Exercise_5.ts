5ac07ae12d5c4253cc0ebe838c41846fa267a5fe
ahsan1001-M <101518516+ahsan1001-M@users.noreply.github.com>
2023-08-03 03:40:47 +0500

    exercise 5
    print string with and without whitespace

---
+++ Exercise_5.ts
@@ -0,0 +1,7 @@
+/* Stripping Names: Store a person’s name, and include some whitespace characters at the beginning and end of the name. 
+Make sure you use each character combination, "\t" and "\n", at least once. 
+Print the name once, so the whitespace around the name is displayed. Then print the name after striping the white spaces.*/
+
+let personName1:string = "\t \n Ahsan\t \n";
+console.log(personName1); //with whitespace
+console.log(personName1.trim()); //without whitspace
