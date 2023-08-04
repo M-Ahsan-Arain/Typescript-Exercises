52fb3d75ed9b1e162293ed54b476ab1f22221722
Ahsan-Altaf <101518516+ahsan1001-M@users.noreply.github.com>
2023-08-04 15:30:55 -0700

    exercise 12
    array of bike names and printing message with calling a name in array.

---
+++ Exercise_12.ts
@@ -0,0 +1,7 @@
+/* Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples.
+ Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”*/
+
+let bikeNames:string[] = ["Honda", "Suzuki", "Yamaha"];
+console.log(`I would like to own a ${bikeNames[0]} motorcycle.`);
+console.log(`I would like to own a ${bikeNames[1]} motorcycle.`);
+console.log(`I would like to own a ${bikeNames[2]} motorcycle.`);
