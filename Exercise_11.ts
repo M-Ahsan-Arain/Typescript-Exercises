5d39ca585a273aa6dfb6d49abc2cb2ea65a873f4
Ahsan-Altaf <101518516+ahsan1001-M@users.noreply.github.com>
2023-08-04 15:26:19 -0700

    exercise 11
    using same array but this time printing same message with calling an each element include in an array.

---
+++ Exercise_11.ts
@@ -0,0 +1,7 @@
+/* Greetings: Start with the array you used in Exercise 10, but instead of just printing each person’s name, print a message to them. 
+The text of each message should be the same, but each message should be personalized with the person’s name.*/
+
+let personNames2:string[] = ["Ali Ammar", "Azeem", "Abdul Hanan"];
+console.log(`Hi ${personNames2[0]}, would you like to go for a dinner?`);
+console.log(`Hi ${personNames2[1]}, would you like to go for a dinner?`);
+console.log(`Hi ${personNames2[2]}, would you like to go for a dinner?`);
