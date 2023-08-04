a02e3c6d861e8dd86a9344899c589b4ad27cf215
Ahsan-Altaf <101518516+ahsan1001-M@users.noreply.github.com>
2023-08-04 15:08:31 -0700

    exercise 9
    Adding comments in simple 2 programs

---
+++ Exercise_9.ts
@@ -0,0 +1,11 @@
+/* Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. 
+If you don’t have anything specific to write because your programs are too simple at this point, 
+just add your name and the current date at the top of each program file. Then write one sentence describing what the program does.*/
+
+let intNumber:number = 5; //storing 5 in variable named intNumber
+
+console.log("square of 5 is: ", intNumber**2); //printing square value of 5
+
+const intNumber2 = 16; //storing 16 in constant variable named intNumber2
+
+console.log("square of 16 is: ",intNumber2**2); //printing square value of 16
