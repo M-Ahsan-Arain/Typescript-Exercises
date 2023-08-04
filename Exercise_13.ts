c11f6f578c2e6e09281c9354e5a7fed3a9c6fe96
Ahsan-Altaf <101518516+ahsan1001-M@users.noreply.github.com>
2023-08-04 15:34:37 -0700

    exercise 13

---
+++ Exercise_13.ts
@@ -0,0 +1,8 @@
+/* Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite? 
+Make a list that includes at least three people you’d like to invite to dinner. 
+Then use your list to print a message to each person, inviting them to dinner.*/
+
+let guestNames:string[] = ["Ali Ammar", "Azeem", "Abdul Hanan"];
+console.log(`Hi ${guestNames[0]}, would you like to go for a dinner?`);
+console.log(`Hi ${guestNames[1]}, would you like to go for a dinner?`);
+console.log(`Hi ${guestNames[2]}, would you like to go for a dinner?`);
