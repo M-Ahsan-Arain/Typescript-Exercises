f12631a14498112df91e6d48b8f197c30042436f
Ahsan-Altaf <101518516+M-Ahsan-Arain@users.noreply.github.com>
2023-08-20 13:00:33 -0700

    object in ts
    defining first object

---
+++ Exercise_20.ts
@@ -0,0 +1,12 @@
+/* think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.*/
+
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 30,
+    isStudent: false
+};
+
+console.log(person.firstName); 
+console.log(person.age);       
+console.log(person.isStudent); 
