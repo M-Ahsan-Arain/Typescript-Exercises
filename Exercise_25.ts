8f326a19a32b7ea4b8951150a932e0df1f5ecf3c
Ahsan-Altaf <101518516+M-Ahsan-Arain@users.noreply.github.com>
2023-08-21 09:19:32 -0700

    if statement with else block

---
+++ Exercise_25.ts
@@ -0,0 +1,12 @@
+/* Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
+• If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
+• If the alien’s color isn’t green, print a statement that the player just earned 10 points.
+• Write one version of this program that runs the if block and another that runs the else block.*/
+
+let alienColor2:string = "red";
+
+if(alienColor2 === "green"){
+    console.log("Congratulations you earned a five points");
+}else{
+    console.log("Congratulations you earned a ten points");
+}
