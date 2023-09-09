d62b26e3c91030fb998c01a6fa5e45c43614e9fc
M Ahsan Altaf <ahsanarain900@gmail.com>
2023-09-09 12:30:38 -0700

    loop through an array

--- Exercise_29.ts
+++ Exercise_29.ts
@@ -6,10 +6,11 @@ in to a website. Loop through the array, and print a greeting to each user:
 
 let usernames: string[] = ["admin", "Ahsan", "Hamza", "Asad", "Hanan"];
 
-for(let i=0;i>usernames.length;i++){
-    if(usernames.includes("admin")){
-        console.log("Hello admin, would you like to see a status report?");
-    } else {
-        console.log(`Hello ${usernames[i]}, thank you for logging in again`);
-    }
+
+if(usernames.includes("admin")){
+    console.log("Hello admin, would you like to see a status report?");
+}
+
+for(let i=1;i<usernames.length;i++){
+    console.log(`Hello ${usernames[i]}, thank you for logging in again`);  
 }
